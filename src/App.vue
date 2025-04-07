6ad32248e69d6c99e92a568dd3886ddc115b5661
alextanhongpin <alex.tan@bandlab.com>
2025-04-08 01:36:00 +0800

    chore: update components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,95 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Image Annotator</h1>
+  <HelloWorld msg="Welcome" @change="handleCanvas" />
+  <input type="file" accept="image/*" multiple @change="handleFile" />
+
+  <button v-if="files.length" @click="handleClear">Clear all</button>
+
+  <div
+    class="img-item"
+    v-for="(file, i) in files"
+    :key="file.name"
+    :class="{ selected: selected === i }"
+  >
+    <img :src="srcs[i]" />
+    <span @click="handleSelect(i)">{{ file.name }}</span>
+    <button @click="handleDelete(i)">Delete</button>
+  </div>
+
+  <div v-if="selected !== null">
+    <h3>Selected Image</h3>
+    <img :src="srcs[selected]" />
+    <p>{{ files[selected].name }}</p>
+    <p>{{ files[selected].size }} bytes</p>
+    <p>{{ files[selected].type }}</p>
+    <p>{{ files[selected].lastModifiedDate }}</p>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import { ref } from "vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const files = ref([]);
+const srcs = ref([]);
+const cache = ref({});
+const selected = ref(null);
+
+async function handleFile(evt) {
+  const newFiles = [];
+  for (let file of evt.target.files) {
+    if (cache.value[file.name]) {
+      continue;
+    }
+    cache.value[file.name] = true;
+    newFiles.push(file);
   }
+  newFiles.sort((a, b) => a.name < b.name);
+  for await (const file of newFiles) {
+    const src = await loadImage(file);
+    srcs.value.push(src);
+  }
+
+  files.value = files.value.concat(newFiles);
+}
+
+function handleSelect(i) {
+  selected.value = i;
+}
+
+function handleClear() {
+  files.value = [];
+  srcs.value = [];
+  cache.value = {};
+  selected.value = null;
+}
+
+function handleDelete(selectedIndex) {
+  const newFiles = [];
+  const newSrcs = [];
+  for (let i = 0; i < files.value.length; i++) {
+    if (i !== selectedIndex) {
+      newFiles.push(files.value[i]);
+      newSrcs.push(srcs.value[i]);
+    }
+  }
+  files.value = newFiles;
+  srcs.value = newSrcs;
+  selected.value = null;
+}
+
+async function loadImage(file) {
+  const reader = new FileReader();
+  return new Promise((resolve) => {
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(file);
+  });
+}
+
+function handleCanvas(rect) {
+  console.log(rect);
 }
 </script>
 
@@ -19,8 +98,26 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.img-item {
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  grid-gap: 10px;
+  &:hover {
+    background: #eee;
+  }
+
+  &.selected {
+    background: #ddd;
+  }
+
+  img {
+    max-width: 100px;
+    max-height: 100px;
+    border: 1px solid #ccc;
+  }
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,103 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <canvas
+      ref="canvas"
+      @mousemove="handleMousemove"
+      @mouseup="handleMouseup"
+      @mousedown="handleMousedown"
+    ></canvas>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+<script setup>
+import { defineEmits, defineProps, ref, useTemplateRef } from "vue";
+const emit = defineEmits(["change"]);
+
+defineProps({
+  msg: {
+    type: String,
+    default: "hi",
+  },
+});
+
+const canvas = useTemplateRef("canvas");
+const isDrawing = ref(false);
+const start = ref({ x: 0, y: 0 });
+
+function handleMousedown(evt) {
+  isDrawing.value = true;
+  start.value = getPosition(evt);
+}
+
+function handleMousemove(evt) {
+  const ctx = canvas.value.getContext("2d");
+  const end = getPosition(evt);
+  clear(ctx);
+  drawGuideLines(ctx, end);
+  if (!isDrawing.value) {
+    return;
   }
+  drawRect(ctx, computeRect(start.value, end));
+}
+
+function handleMouseup(evt) {
+  isDrawing.value = false;
+  const end = getPosition(evt);
+  const rect = computeRect(start.value, end);
+  if (isValidRect(rect)) {
+    emit("change", rect);
+  }
+}
+
+function clear(ctx) {
+  ctx.clearRect(0, 0, canvas.value.width, canvas.value.height);
+}
+
+function drawGuideLines(ctx, { x, y }) {
+  ctx.strokeStyle = "rgba(0, 0, 0, 0.5)";
+  ctx.lineWidth = 1;
+
+  // Vertical line
+  ctx.beginPath();
+  ctx.moveTo(x, 0);
+  ctx.lineTo(x, canvas.value.height);
+  ctx.stroke();
+
+  // Horizontal line
+  ctx.beginPath();
+  ctx.moveTo(0, y);
+  ctx.lineTo(canvas.value.width, y);
+  ctx.stroke();
+}
+
+function isValidRect({ x, y, width, height }) {
+  return x >= 0 && y >= 0 && width > 10 && height > 10;
+}
+
+function computeRect(p1, p2) {
+  const x = Math.min(p1.x, p2.x);
+  const y = Math.min(p1.y, p2.y);
+  const width = Math.abs(p1.x - p2.x);
+  const height = Math.abs(p1.y - p2.y);
+
+  return { x, y, width, height };
+}
+
+function drawRect(ctx, { x, y, width, height }) {
+  ctx.strokeStyle = "blue";
+  ctx.lineWidth = 2;
+  ctx.strokeRect(x, y, width, height);
+}
+
+function getPosition(evt) {
+  const rect = canvas.value.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top,
+  };
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
